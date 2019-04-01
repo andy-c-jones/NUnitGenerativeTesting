4e9ba7fe44a8aae36171ac71a3448717a2705bd6
Andrew Jones <acjones@protonmail.com>
2019-04-01 21:33:21 +0100

    Initial commit

---
+++ NUnitGenerativeTesting.Tests/RandomStringAttributeTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace NUnitGenerativeTesting.Tests
+{
+    [TestFixture]
+    public class RandomStringAttributeTests
+    {
+        [Test]
+        public void should_generate_a_string([RandomString] string randomString)
+        {
+            Assert.That(randomString, Is.Not.Null);
+        }
+
+        [Test]
+        public void should_generate_data_x_times_where_x_is_the_specified_count([Random(0, 100, 10)] int count)
+        {
+            var values = new RandomStringAttribute(count: count)
+                .GetData(new FakeParameterInfo()).Cast<string>().ToList();
+            Assert.That(values.Count, Is.EqualTo(count));
+        }
+
+        [Test]
+        public void length_should_always_be_less_than_default_of_255([RandomString(count: 100)] string randomString)
+        {
+            Assert.That(randomString.Length, Is.LessThanOrEqualTo(255));
+        }
+
+        private readonly List<string> _alreadySeen = new List<string>();
+        [Test]
+        public void should_be_random_aka_not_generating_the_same_string_too_often(
+            [RandomString(count: 1000)] string randomString)
+        {
+            _alreadySeen.Add(randomString);
+            _alreadySeen.GroupBy(value => value).ToList().ForEach(g =>
+                Assert.That(g.Count(), Is.LessThanOrEqualTo(10)));
+        }
+
+        [Test]
+        public void length_of_strings_should_vary_between_min_and_max(
+            [Random(0, 250, 10)] int min,
+            [Random(251, 500, 10)] int max)
+        {
+            var randomStrings = new RandomStringAttribute(min, max)
+                .GetData(new FakeParameterInfo());
+            var values = randomStrings.Cast<string>().ToList();
+            values.ForEach(s =>
+            {
+                Assert.That(s.Length, Is.GreaterThanOrEqualTo(min));
+                Assert.That(s.Length, Is.LessThanOrEqualTo(max));
+            });
+            
+            values.GroupBy(value => value.Length).ToList().ForEach(g =>
+                Assert.That(g.Count(), Is.LessThanOrEqualTo(5)));
+        }
+        
+        [Test]
+        public void length_of_strings_should_be_varied()
+        {
+            var randomStrings = new RandomStringAttribute(0, 500, 1000)
+                .GetData(new FakeParameterInfo());
+            var values = randomStrings.Cast<string>().ToList();
+            
+            values.GroupBy(value => value.Length).ToList().ForEach(g =>
+                Assert.That(g.Count(), Is.LessThanOrEqualTo(10), $"{g.First().Length} found {g.Count()} times."));
+        }
+
+        //todo: allowedChars
+
+        //todo: regex
+    }
+
+    public class FakeParameterInfo : IParameterInfo
+    {
+        public T[] GetCustomAttributes<T>(bool inherit) where T : class => throw new NotImplementedException();
+        public bool IsDefined<T>(bool inherit) where T : class => throw new NotImplementedException();
+        public bool IsOptional { get; }
+        public IMethodInfo Method { get; }
+        public ParameterInfo ParameterInfo { get; }
+        public Type ParameterType { get; }
+    }
+}
---
+++ NUnitGenerativeTesting/RandomStringAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+
+namespace NUnitGenerativeTesting
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public class RandomStringAttribute : NUnitAttribute, IParameterDataSource
+    {
+        private readonly int _min;
+        private readonly int _max;
+        private readonly int _count;
+        private readonly Random _random = new Random();
+
+        public RandomStringAttribute(int min = 0, int max = 255, int count = 5)
+        {
+            _min = min;
+            _max = max;
+            _count = count;
+        }
+
+        public IEnumerable GetData(IParameterInfo parameter)
+        {
+            for (var i = 0; i < _count; i++)
+            {
+                yield return GenerateRandomString();
+            }
+        }
+
+        private object GenerateRandomString()
+        {
+            var randomLength = _random.Next(_min, _max);
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var stringChars = new char[randomLength];
+
+            for (var j = 0; j < stringChars.Length; j++)
+            {
+                stringChars[j] = chars[_random.Next(chars.Length)];
+            }
+
+            return new string(stringChars);
+        }
+    }
+}
